865fc191c2bd2100fc731fb5fb5021be3ceae73c
Eli Wallace <etw31300@gmail.com>
2025-02-20 20:32:49 -0600

    fix: added unit test

---
+++ src/core/providers/logger.provider.test.ts
@@ -0,0 +1,22 @@
+import { LoggerService } from '@nestjs/common'
+import BunyanLogger from 'bunyan'
+import Logger from './logger.provider'
+
+describe('logger.provider', () => {
+  describe('Logger', () => {
+    const loggerInstance = new Logger()
+
+    it('should be an instance of the bunyan logger when instantiated', () => {
+      expect(loggerInstance).toBeInstanceOf(BunyanLogger)
+    })
+
+    it.each<keyof LoggerService | keyof BunyanLogger>([
+      'info',
+      'debug',
+      'warn',
+      'error'
+    ])('should expose the \'%s\' function when instantiated', (logFunction) => {
+      expect(typeof loggerInstance[logFunction]).toBe('function')
+    })
+  })
+})
