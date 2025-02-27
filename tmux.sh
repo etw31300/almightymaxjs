ecda7bd7874d0fee3a2f8a06595d447d7933be57
Eli Wallace <etw31300@gmail.com>
2025-02-26 22:20:48 -0600

    fix: update to tmux environment

--- tmux.sh
+++ tmux.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 session_name="almightymaxjs"
 
+# function to determine if we already have a pane open already
+function has_pane() {
+  tmux has-session -t "$session_name.0$1" 2> /dev/null
+}
+
 # kill the pre-existing tmux session if one exists
 if tmux has-session -t $session_name; then
   tmux kill-session -t $session_name
@@ -9,11 +14,21 @@ fi
 tmux new-session -s $session_name -d
 tmux rename-window $session_name
 
-tmux select-window -t 0
-tmux split-window -h
-tmux send-keys -t 1 "npm run dev:lavalink" ENTER
+tmux select-window -t "$session_name.0"
+
+# if we don't have pane 01 (lavalink), create it
+if ! has_pane 1; then
+  tmux split-window -h
+  tmux select-pane -t "$session_name.01" -T "Lavalink"
+fi
+tmux send-keys -t "$session_name.01" "npm run dev:lavalink" ENTER
 
 # wait for lavalink to be set up and ready for connections before running the nest service
 while ! nc -z localhost 2333 </dev/null; do sleep 5; done
-tmux split-window -v -t 1
-tmux send-keys -t 2 "npm run dev:nest" ENTER
+
+# if we don't have pane 02 (nest), create it
+if ! has_pane 2; then
+  tmux split-window -v -t "$session_name.01"
+  tmux select-pane -t "$session_name.02" -T "NestJS"
+fi
+tmux send-keys -t "$session_name.02" "npm run dev:nest" ENTER
